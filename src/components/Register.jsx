a665c18500cd1fe558f3d2a214856b88c941e202
Naman Bansal <namanbansal102@gmail.com>
2024-10-29 21:38:47 +0530

    Added Account Register and account balance

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Web3 from 'web3';
+const web3=new Web3(window.ethereum )
 
 const Register = () => {
+    const [account, setAccount] = useState("")
+    const [balance, setBalance] = useState("")
+    const [status, setStatus] = useState("")
+    useEffect(() => {
+        checkConnection();
+      }, []);
+    
+      async function checkConnection() {
+        if (typeof window.ethereum !== "undefined") {
+          try {
+            const accounts = await window.ethereum.request({ method: "eth_accounts" });
+            if (accounts.length > 0) {
+              setAccount(accounts[0]);
+                let balance=await web3.eth.getBalance(accounts[0])
+                balance = web3.utils.fromWei(balance, 'ether'); // Convert from Wei to Ether
+console.log("Balance is:", balance);
+                
+                setBalance(balance)
+              setStatus("Disconnect");
+            }
+          } catch (error) {
+            console.error("Error checking connection:", error);
+          }
+        }
+      }
+    
   return (
     <div className='flex flex-col justify-center items-center gap-7 text-yellow-400 text-2xl'>
         <center className='text-3xl text-yellow-400 font-bold'>
@@ -10,12 +38,12 @@ const Register = () => {
 
         <div>
             <h1 className='font-bold'>Your Account Address</h1>
-            <h2 className='text-xl'>0xbC2b806d11c1959E709A3b6d20188140275EE364</h2>
+            <h2 className='text-xl'>{account}</h2>
         </div>
         <br />
         <div>
             <h1 className='font-bold'>Your Account Balance</h1>
-            <h2 className='text-xl'>0.00002ETH</h2>
+            <h2 className='text-xl'>{balance}</h2>
         </div>
         <br />
         <div className='flex flex-col'>
